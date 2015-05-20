f4abc532241eacaa50384cfcadef0ece804c371d
Viradj <viradj_13@hotmail.com>
2015-05-20 11:14:12 +0200

    test

--- src/nl/saxion/network_services/MainActivity.java
+++ src/nl/saxion/network_services/MainActivity.java
@@ -12,6 +12,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	
+	// hoi casper
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
